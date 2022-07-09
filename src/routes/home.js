c21b9e3e9de688f8ab044aef2b6ff9b17cc00e24
akshat bhardwaj <bhardwajakshat842@gmail.com>
2022-07-09 19:44:15 +0530

    ejs bug

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,7 +6,7 @@ const home=express.Router();
 const mongoose=require('mongoose');
 
 // api key : 8f520d48e32f1c66bbb9e17300dc5258
-const API_KEY= process.env.API_KEY;
+const API_KEY=process.env.API_KEY;
 const BASE_URL= "https://api.themoviedb.org/3";
 const popular =BASE_URL+ "/discover/movie?sort_by=popularity.desc&"+ API_KEY;
 const bollywood =BASE_URL+"/discover/movie?"+ API_KEY+"&language=en-US&sort_by=popularity.desc&page=1&primary_release_year=2021&with_original_language=hi";
@@ -21,6 +21,8 @@ request(popular,function(error,response,body){
     var data=JSON.parse(body);
     mp=data.results;
     console.log("pop");
+    console.log(mp);
+    console.log(popular);
 });
 request(bollywood,function(error,response,body){
     var data=JSON.parse(body);
